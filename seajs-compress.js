b16600dedf24cd705198e11b39717cda4ccd6e6e
webryan <webryan@foxmail.com>
2013-06-20 22:05:47 +0800

    Update seajs-compress.js

--- seajs-compress.js
+++ seajs-compress.js
@@ -1,57 +1,60 @@
 var fs = require('fs');
 var gcc = require('closure-compiler')
 
-var args = process.argv.splice(2);
-if (args.length!=2){
-  console.log('usage:node grunt-sea.js src-path to-path');
-	return;
-}
-var srcFile = args[0];
-var toFile = args[1];
-var preFunction = 'function require(){alert(arguments.length)}';
+exports.compress = function(srcFile,toFile,callback,option){
+	if (!(srcFile&&toFile)){
+		console.log('usage:node grunt-sea.js src-path to-path callback compress_option');
+		return;
+	}
+	callback = callback || function(){};
+	option = option || {};
 
-fs.exists(srcFile, function (flag) {
-	if (flag){
-		fs.readFile(srcFile, "utf-8",function (err, data) {
-			if (err) throw err;
-			var content = data;
-			//replace require and make require a global function
-			content = content.replace(/define\(function\(([\w\s,]*)\)/ig, function(a,b){
-				if (!b) return a;
-				var arr = b.split(',');
-				arr[0]='henry';
-				return a.replace(b,arr.join(','));
-			});
-			content = preFunction + content; 
-
-			fs.writeFile(toFile, content, {encoding:'utf8',flag:'w+'}, function (err) {
+	var preFunction = 'function require(){alert(arguments.length)}';
+	fs.exists(srcFile, function (flag) {
+		if (flag){
+			fs.readFile(srcFile, "utf-8",function (err, data) {
 				if (err) throw err;
-				gcc.compile(fs.readFileSync(toFile), {}, function(err, stdout, stderr){
+				var content = data;
+				//replace require and make require a global function
+				content = content.replace(/define\(function\(([\w\s,]*)\)/ig, function(a,b){
+					if (!b) return a;
+					var arr = b.split(',');
+					arr[0]='henry';
+					return a.replace(b,arr.join(','));
+				});
+				content = preFunction + content; 
+
+				fs.writeFile(toFile, content, {encoding:'utf8',flag:'w+'}, function (err) {
 					if (err) throw err;
-					//delete global require function and replace define function's first argument
-					var content = stdout.replace(preFunction,'');
-					content =  content.replace(/define\(function\(([\w\s,]*)\)/ig, function(a,b){
-						if (!b){
-							return a.replace('function()','function(require)') ;	
-						}else{
-							var first = b.split(',')[0];
-							return a.replace(b,b.replace(first,'require'))
-						} 
-						
+					gcc.compile(fs.readFileSync(toFile), {}, function(err, stdout, stderr){
+						if (err) throw err;
+						//delete global require function and replace define function's first argument
+						var content = stdout.replace(preFunction,'');
+						content =  content.replace(/define\(function\(([\w\s,]*)\)/ig, function(a,b){
+							if (!b){
+								return a.replace('function()','function(require)') ;	
+							}else{
+								var first = b.split(',')[0];
+								return a.replace(b,b.replace(first,'require'))
+							} 
+
+						});
+
+						fs.writeFile(toFile, content, {encoding:'utf8',flag:'w+'}, function (err) {
+							if (err) throw err;
+							callback(true,'['+toFile+'] '+srcFile+' is compressed.');
+						});
+
 					});
 
-					fs.writeFile(toFile, content, {encoding:'utf8',flag:'w+'}, function (err) {
-						if (err) throw err;
-						console.log( '['+toFile+'] '+srcFile+' is compressed.');
-					});
-						
+
 				});
 
+			});		
+		}else{
+			callback(false,'error: src-path is not exist!');
+		}
+	});
+}
 
-			});
-			
-		});		
-	}else{
-		console.log('error: src-path is not exist!');
-	}
-});
+
